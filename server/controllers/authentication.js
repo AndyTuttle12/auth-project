4a42f6d201b20c91d947ecda5c8b1f3453e8d804
Andy Tuttle <andytuttle12@gmail.com>
2017-07-24 21:26:33 -0400

    Finished build of basic auth server with signup, signin, and auth'd routes.

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -7,12 +7,16 @@ function tokenForUser(user) {
     return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+    res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
     if (!email || !password) {
-        return res.status(422).send({ error: 'You must provide email and password '});
+        return res.status(422).send({ error: 'You must provide email and password' });
     }
 
     User.findOne({ email: email }, function(err, existingUser) {
