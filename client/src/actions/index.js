2db4ca284ad6c80ca07d674d9db0004efa16698f
Andy Tuttle <andytuttle12@gmail.com>
2017-08-02 23:08:47 -0400

    Final steps to add automatic routing, and accessing protected server routes.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -44,4 +44,18 @@ export function authError(error) {
         type: AUTH_ERROR,
         payload: error
     };
+}
+
+export function fetchMessage() {
+    return function(dispatch) {
+        axios.get(ROOT_URL, {
+            headers: { authorization: localStorage.getItem('token') }
+        })
+            .then(response => {
+                dispatch({
+                    type: FETCH_MESSAGE,
+                    payload: response.data.message
+                });
+            });
+    }
 }
